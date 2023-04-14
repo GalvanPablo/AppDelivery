763d2dfaa8e652351e03af42fddab164b66b20d1
Pablo Galvan <pablogalvan.015@gmail.com>
2023-04-14 14:54:46 -0300

    Pre datos usuario

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Home = () => {
+  const userId = useSelector(state => state.auth.userId)
   return (
     <View>
       <Text>Home</Text>
+      <Text>LocalID: {userId}</Text>
     </View>
   )
 }
